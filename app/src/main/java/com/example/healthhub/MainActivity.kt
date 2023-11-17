ea52adaf18b028592da85a66f03223e85bdddb11
jooyyoo <aliceyun9@naver.com>
2023-11-17 16:58:51 +0900

    [init] Binding 추가로 인한 MainActivity 변경

--- app/src/main/java/com/example/healthhub/MainActivity.kt
+++ app/src/main/java/com/example/healthhub/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.healthhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.healthhub.databinding.ActivityMainBinding
+import com.example.healthhub.util.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
