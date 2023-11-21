fe6a11b60fd3f468179e741648ceebf4d61dac8d
jooyyoo <aliceyun9@naver.com>
2023-11-21 17:28:57 +0900

    [feat] viewModelFactory 구현
    viewModel 에 파라미터가 있을 경우

---
+++ app/src/main/java/com/example/healthhub/data/LocalData.kt
@@ -0,0 +1,34 @@
+package com.example.healthhub.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.example.healthhub.BuildConfig
+
+class LocalData(context: Context) {
+    private val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val dataStore: SharedPreferences =
+        if (BuildConfig.DEBUG) context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        else EncryptedSharedPreferences.create(
+            context,
+            FILE_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+    var nickname: String
+        set(value) = dataStore.edit { putString(USER_NICKNAME, value) }
+        get() = dataStore.getString(USER_NICKNAME, "") ?: ""
+
+
+    companion object {
+        const val FILE_NAME = "HealthHub"
+        const val USER_NICKNAME = "nickname"
+    }
+}
