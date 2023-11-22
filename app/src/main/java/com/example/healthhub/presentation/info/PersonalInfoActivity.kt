1562e27946b70b2827c4f2899fa7b4494beda6f9
jooyyoo <aliceyun9@naver.com>
2023-11-22 12:00:57 +0900

    [feat] PersonalInfoActivity 로직 구현
    완료 버튼 클릭 시 내부에 데이터 저장, 다음 뷰로 이동할 수 있도록 intent 추가

--- app/src/main/java/com/example/healthhub/presentation/info/PersonalInfoActivity.kt
+++ app/src/main/java/com/example/healthhub/presentation/info/PersonalInfoActivity.kt
@@ -1,21 +1,48 @@
 package com.example.healthhub.presentation.info
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.example.healthhub.R
+import com.example.healthhub.common.ViewModelFactory
 import com.example.healthhub.databinding.ActivityPersonalInfoBinding
-import com.example.healthhub.util.binding.BindingActivity
+import com.example.healthhub.presentation.water.HydrationActivity
 import com.example.healthhub.util.extension.hideKeyboard
+import com.example.healthhub.util.binding.BindingActivity
 
-class PersonalInfoActivity: BindingActivity<ActivityPersonalInfoBinding>(R.layout.activity_personal_info) {
+class PersonalInfoActivity :
+    BindingActivity<ActivityPersonalInfoBinding>(R.layout.activity_personal_info) {
+    private val viewModel: PersonalInfoViewModel by viewModels { ViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         addListeners()
     }
 
-    private fun addListeners(){
-        binding.root.setOnClickListener{
+    private fun addListeners() {
+        binding.root.setOnClickListener {
             hideKeyboard(it)
         }
+        binding.btnAllReady.setOnClickListener {
+            Log.e("button click","${viewModel.nickname.value}")
+            moveToHome()
+            viewModel.savePersonalInfo()
+        }
+    }
+
+    private fun moveToHome() {
+        Log.e("home click","${viewModel.nickname.value}")
+        val intent = Intent(this, HydrationActivity::class.java)
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
+    companion object {
+        const val PERSONAL_INFO = "PERSONAL_INFO"
     }
 }
