abdad13991d1bdfc797642a1b9ecfb27b54c42d4
gogyeong-a <gyeongago1@gmail.com>
2023-11-23 14:30:21 +0900

    [test] 커밋 테스트

--- app/src/main/java/com/example/healthhub/presentation/calorie/CalorieActivity.kt
+++ app/src/main/java/com/example/healthhub/presentation/calorie/CalorieActivity.kt
@@ -1,6 +1,8 @@
 package com.example.healthhub.presentation.calorie
 
+import com.example.healthhub.R
+import com.example.healthhub.databinding.ActivityCalorieMenuBinding
 import com.example.healthhub.util.binding.BindingActivity
 
-class CalorieActivity: BindingActivity<>(){
+class CalorieActivity : BindingActivity<ActivityCalorieMenuBinding>(R.layout.activity_calorie_menu){
 }
