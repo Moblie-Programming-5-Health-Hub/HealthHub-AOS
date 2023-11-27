e672bf8b9774545ac9d5e571ea8e795b41aefb40
jooyyoo <aliceyun9@naver.com>
2023-11-27 20:57:44 +0900

    [feat] 완료 버튼 클릭 조건 만족 시 뷰 넘어가도록 구현

--- app/src/main/java/com/example/healthhub/presentation/info/PersonalInfoActivity.kt
+++ app/src/main/java/com/example/healthhub/presentation/info/PersonalInfoActivity.kt
@@ -6,11 +6,11 @@ import android.util.Log
 import androidx.activity.viewModels
 import com.example.healthhub.R
 import com.example.healthhub.common.ViewModelFactory
-import com.example.healthhub.data.LocalData
 import com.example.healthhub.databinding.ActivityPersonalInfoBinding
 import com.example.healthhub.presentation.water.HydrationActivity
-import com.example.healthhub.util.extension.hideKeyboard
 import com.example.healthhub.util.binding.BindingActivity
+import com.example.healthhub.util.extension.hideKeyboard
+import com.google.android.material.snackbar.Snackbar
 
 class PersonalInfoActivity :
     BindingActivity<ActivityPersonalInfoBinding>(R.layout.activity_personal_info) {
@@ -29,14 +29,21 @@ class PersonalInfoActivity :
             hideKeyboard(it)
         }
         binding.btnAllReady.setOnClickListener {
-            Log.e("button click","${viewModel.nickname.value}")
-            viewModel.savePersonalInfo()
-            moveToHome()
+            Log.e("완료 버튼 클릭", "${viewModel.nickname.value}")
+            Log.e("완료 버튼 클릭","${viewModel.gender.value}")
+            if (viewModel.readyToStart()) {
+                viewModel.savePersonalInfo()
+                moveToHome()
+            } else {
+                Snackbar.make(binding.root, "모두 입력해주세요.", Snackbar.LENGTH_SHORT).apply {
+                    anchorView = binding.btnAllReady
+                }.show()
+            }
         }
     }
 
     private fun moveToHome() {
-        Log.e("home click","${viewModel.nickname.value}")
+        Log.e("완료 버튼 클릭 후 페이지 이동", "${viewModel.nickname.value}")
         val intent = Intent(this, HydrationActivity::class.java)
         startActivity(intent)
         finish()
