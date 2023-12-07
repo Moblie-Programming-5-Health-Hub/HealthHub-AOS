903c54d389390c5375e4a524eba339c9add0c7b0
jooyyoo <aliceyun9@naver.com>
2023-12-07 13:37:49 +0900

    [chore] 코드 정렬

--- app/src/main/java/com/example/healthhub/presentation/water/HydrationActivity.kt
+++ app/src/main/java/com/example/healthhub/presentation/water/HydrationActivity.kt
@@ -6,15 +6,15 @@ import com.example.healthhub.databinding.ActivityWatercheckBinding
 import com.example.healthhub.util.binding.BindingActivity
 import com.example.healthhub.util.extension.hideKeyboard
 
-class HydrationActivity:BindingActivity<ActivityWatercheckBinding>(R.layout.activity_watercheck) {
+class HydrationActivity : BindingActivity<ActivityWatercheckBinding>(R.layout.activity_watercheck) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         addListeners()
     }
 
-    private fun addListeners(){
-        binding.root.setOnClickListener{
+    private fun addListeners() {
+        binding.root.setOnClickListener {
             hideKeyboard(it)
         }
     }
